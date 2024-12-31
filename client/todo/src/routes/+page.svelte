9f27ef0a65dfc0747153c512fad51b07978fd8a2
Amaresha <amaresham50@gmail.com>
2024-12-31 15:13:12 +0530

    content type remove

--- client/todo/src/routes/+page.svelte
+++ client/todo/src/routes/+page.svelte
@@ -83,9 +83,7 @@
 		try {
 			const response = await fetch('http://localhost:8000/todos/delete', {
 				method: 'DELETE',
-				headers: {
-					'Content-Type': 'application/json'
-				},
+
 				body: JSON.stringify({ id })
 			});
 			if (!response.ok) throw new Error('Failed to delete todo');
